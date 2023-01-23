344d510c24894ed291e41deabd51404166dec337
estimated1337 <91435835+estimated1337@users.noreply.github.com>
2023-01-24 01:01:56 +0300

    quality code changes

--- hidden_syscall/hidden_syscall.asm
+++ hidden_syscall/hidden_syscall.asm
@@ -4,19 +4,14 @@ EXTERN ntdll_stub: QWORD
 
 syscall_stub proc
 	mov r10, rcx
-	mov eax, [rsp + 20h + 8] 
+	mov eax, [rsp + 28h] 
 	pop r12 
-	db 4ch ; lea r11, [rip + 0xf]
-	db 8dh
-	db 1dh
-	db 0fh
-	db 00h
-	db 00h
-	db 00h
+	lea r11, [ret_address]
 	mov [rsp], r11
 	mov r11, [ntdll_stub]
 	jmp r11
 	ret
+ret_address:
 	sub rsp, 10h
 	mov [rsp], r12
 	ret
